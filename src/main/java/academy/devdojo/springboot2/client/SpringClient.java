74c9f41b3b20228e3c3e18c4c5d97ea54bf55db9
fabioamaral <fabiokamaral@gmail.com>
2023-04-10 11:33:00 -0400

    RestTemplate parte 2. Requisições POST.

--- src/main/java/academy/devdojo/springboot2/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -3,7 +3,9 @@ package academy.devdojo.springboot2.client;
 import academy.devdojo.springboot2.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,23 +16,43 @@ import java.util.List;
 public class SpringClient {
 
     public static void main(String[] args) {
+        //#################
+        // GET
+        //##############################################################################################################
         ResponseEntity<Anime> entityIdOnUrl = new RestTemplate().getForEntity("http://localhost:8080/animes/2", Anime.class);
-        log.info(entityIdOnUrl);
+        log.info("entityIdOnUrl: {}", entityIdOnUrl);
 
         ResponseEntity<Anime> entityIdAsParameter = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 4);
-        log.info(entityIdAsParameter);
+        log.info("entityIdAsParameter: {}" ,entityIdAsParameter);
 
         Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/2", Anime.class);
-        log.info(object);
+        log.info("object: {}", object);
 
         Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
-        log.info(Arrays.toString(animes));
+        log.info("animes: {}", Arrays.toString(animes));
 
         ResponseEntity<List<Anime>> animeList = new RestTemplate().exchange(
                 "http://localhost:8080/animes/all",
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<>() {});
-        log.info(animeList.getBody());
+        log.info("animeList: {}", animeList.getBody());
+        //##############################################################################################################
+
+        //#################
+        // POST
+        //##############################################################################################################
+        Anime kingdom = Anime.builder().name("kingdom").build();
+        Anime kingdomSaved = new RestTemplate().postForObject("http://localhost:8080/animes", kingdom, Anime.class);
+        log.info("kingdomSaved: {}", kingdomSaved);
+
+        Anime samuraiChamploo = Anime.builder().name("Samurai Champloo").build();
+        ResponseEntity<Object> samuraiChamplooSaved = new RestTemplate().exchange("http://localhost:8080/animes",
+                HttpMethod.POST,
+                new HttpEntity<>(samuraiChamploo),
+                new ParameterizedTypeReference<>() {
+                });
+        log.info("samuraiChamplooSaved: {}", samuraiChamplooSaved.getBody());
+        //##############################################################################################################
     }
 }
