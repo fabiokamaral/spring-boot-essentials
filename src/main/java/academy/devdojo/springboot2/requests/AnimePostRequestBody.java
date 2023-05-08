38142b791ae0ac99d0dcb2a8d899cbb2f0d8ee9e
fabioamaral <fabiokamaral@gmail.com>
2023-05-08 13:07:58 -0400

    Spring OpenAPI Swagger adicionando dependência e configurando informações da documentação gerada.

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,11 +1,14 @@
 package academy.devdojo.springboot2.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 @Data
 @Builder
@@ -14,6 +17,9 @@ import javax.validation.constraints.NotEmpty;
 public class AnimePostRequestBody {
 
     @NotEmpty(message = "The anime name cannot be empty")
+    @Schema(description = "This is de Anime's name",
+            example = "Tensei Shittara Slime Datta Ken",
+            requiredMode = REQUIRED)
     private String name;
 
 }
